eda42d04f7b1324b400e62ebfecbec7a08bcc1f5
underengineering <san4a852b@gmail.com>
2023-04-04 12:44:05 +0000

    Add vdpau and vaapi to opengl pkgs

--- modules/system/wayland/default.nix
+++ modules/system/wayland/default.nix
@@ -21,6 +21,10 @@ in
       enable = true;
       driSupport = true;
       driSupport32Bit = true;
+      extraPackages = with pkgs; [
+        vaapiVdpau
+        libvdpau-va-gl
+      ];
     };
   };
 }
