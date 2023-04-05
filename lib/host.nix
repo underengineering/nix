8f87af0024f55e23dfdc9f241c4543642bae613f
underengineering <san4a852b@gmail.com>
2023-04-05 14:05:29 +0000

    Use flake nixpkgs systemwide

--- lib/host.nix
+++ lib/host.nix
@@ -69,6 +69,13 @@ with builtins; with utils;
               "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
             ];
           };
+          nix.registry.nixpkgs.flake = inputs.nixpkgs;
+          nix.nixPath = [
+            "nixpkgs=/etc/channels/nixpkgs"
+            "nixos-config=/etc/nixos/configuration.nix"
+            "/nix/var/nix/profiles/per-user/root/channels"
+          ];
+          environment.etc."channels/nixpkgs".source = inputs.nixpkgs.outPath;
 
           system.stateVersion = "23.05";
         }
