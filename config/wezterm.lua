4d11c03323e59b7c97070b0dfa65e391a2917731
underengineering <san4a852b@gmail.com>
2023-04-10 19:09:45 +0300

    [wezterm] Disable window padding

--- config/wezterm.lua
+++ config/wezterm.lua
@@ -10,4 +10,11 @@ config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = true
 
+config.window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0
+}
+
 return config
