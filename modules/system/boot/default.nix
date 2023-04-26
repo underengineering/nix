e438b6a8a4f0a6c266d8d336345177feb364880b
underengineering <san4a852b@gmail.com>
2023-04-26 21:05:39 +0300

    Add noatime to mount options

--- modules/system/boot/default.nix
+++ modules/system/boot/default.nix
@@ -11,10 +11,12 @@ in
     fileSystems."/" = {
       device = "/dev/disk/by-label/NIXROOT";
       fsType = "ext4";
+      options = [ "noatime" "nodiratime" ];
     };
     fileSystems."/boot" = {
       device = "/dev/disk/by-label/NIXBOOT";
       fsType = "vfat";
+      options = [ "noatime" "nodiratime" ];
     };
     boot = {
       loader = {
