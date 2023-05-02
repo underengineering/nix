d96e6c67dc89f007af2358792a4290febe854b6e
underengineering <san4a852b@gmail.com>
2023-05-02 18:39:57 +0300

    [apply.sh] Require zsh & add very important msg

--- apply.sh
+++ apply.sh
@@ -1,18 +1,22 @@
-#!/usr/bin/env sh
+#!/usr/bin/env zsh
 
 set -e
+
+BLUE='\033[0;34m'
+RESET='\033[0m'
+SYMBOL="${BLUE}󰕹${RESET}"
 
 TARGET=$1
 case $TARGET in
     "system")
         HOST_NAME=$2
         ARGS="${@:3}"
-        echo "Building system configuration for host '$HOST_NAME'"
+        echo "$SYMBOL Building system configuration for host '$HOST_NAME' $SYMBOL"
         sudo nixos-rebuild switch --flake ".#$HOST_NAME" $ARGS
         ;;
     "user")
         ARGS="${@:2}"
-        echo "Building user configuration for user '$USER'"
+        echo "$SYMBOL Building user configuration for user '$USER' $SYMBOL"
         nix build --impure ".#homeManagerConfigurations.$USER.activationPackage" $ARGS
         ./result/activate
         ;;
