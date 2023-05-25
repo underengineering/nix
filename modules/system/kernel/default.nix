0d269c8f4694110a018d35c5e3833e53eb982af5
underengineering <san4a852b@gmail.com>
2023-05-25 17:10:43 +0300

    Add more options to kernel module

--- modules/system/kernel/default.nix
+++ modules/system/kernel/default.nix
@@ -18,32 +18,68 @@ in {
       type = types.enum ["intel" "amd"];
     };
     disableMitigations = mkOption {
-      description = "Whether to disable performance-killing mitigations";
+      description = "Whether to disable exploit mitigations";
       type = types.bool;
       default = true;
     };
+    reducedDebugInfo = mkOption {
+      description = "Reduces debugging information";
+      type = types.bool;
+      default = true;
+    };
+    disableSecurity = mkOption {
+      description = "Disables security module support";
+      type = types.bool;
+      default = true;
+    };
+    patches = mkOption {
+      description = "Kernel patches";
+      type = with types; listOf attrs;
+      default = [];
+    };
   };
   config = mkIf (cfg.enablePatches) {
-    boot.kernelPatches = [
-      {
-        name = "CPU-specific optimizations";
-        patch = null;
-        extraStructuredConfig = mkMerge [
-          (mkIf (cfg.cpuVendor == "intel") {
-            MNATIVE_INTEL = kernel.yes;
-          })
-          (mkIf (cfg.cpuVendor == "amd") {
-            MNATIVE_AMD = kernel.yes;
-          })
-        ];
-      }
-      (mkIf (cfg.disableMitigations) {
-        name = "Disable mitigations";
-        patch = null;
-        extraStructuredConfig = {
-          SPECULATION_MITIGATIONS = kernel.no;
-        };
-      })
-    ];
+    boot.kernelPatches =
+      [
+        {
+          name = "CPU-specific optimizations";
+          patch = null;
+          extraStructuredConfig = mkMerge [
+            (mkIf (cfg.cpuVendor == "intel") {
+              MNATIVE_INTEL = kernel.yes;
+            })
+            (mkIf (cfg.cpuVendor == "amd") {
+              MNATIVE_AMD = kernel.yes;
+            })
+          ];
+        }
+        (mkIf (cfg.disableMitigations) {
+          name = "Disable mitigations";
+          patch = null;
+          extraStructuredConfig = {
+            SPECULATION_MITIGATIONS = kernel.no;
+            STACKPROTECTOR_STRONG = kernel.no;
+          };
+        })
+        (mkIf (cfg.reducedDebugInfo) {
+          name = "Reduce debugging information";
+          patch = null;
+          extraStructuredConfig = {
+            DEBUG_INFO_REDUCED = mkForce kernel.yes;
+          };
+        })
+        (mkIf (cfg.disableSecurity) {
+          name = "Disable SELinux";
+          patch = null;
+          extraStructuredConfig = with kernel; {
+            # SECURITY = no;
+            SECURITY_APPARMOR = mkForce no;
+            SECURITY_LANDLOCK = mkForce no;
+            SECURITY_LOCKDOWN_LSM = mkForce no;
+            DEFAULT_SECURITY_APPARMOR = mkForce (option no);
+          };
+        })
+      ]
+      ++ cfg.patches;
   };
 }
