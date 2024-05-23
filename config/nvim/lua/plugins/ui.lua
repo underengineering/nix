81264a8e810e122424a58c6151008c67d3544a2b
underengineering <san4a852b@gmail.com>
2024-05-23 21:04:49 +0300

    refactor(nvim): Nuke neo-tree

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -44,22 +44,6 @@ return {
         },
         config = require "plugins/configs/telescope",
     },
-    --[[ {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
-        cmd = "Neotree",
-        dependencies = {
-            -- "stevearc/oil.nvim",
-            "nvim-lua/plenary.nvim",
-            "kyazdani42/nvim-web-devicons",
-            "MunifTanjim/nui.nvim",
-            "antosha417/nvim-lsp-file-operations"
-        },
-        keys = {
-            { "<Leader>b", "<Cmd>Neotree<CR>", desc = "Open neo-tree", silent = true }
-        },
-        config = require "plugins.configs.neo-tree",
-    }, ]]
     {
         "MagicDuck/grug-far.nvim",
         cmd = "GrugFar",
