a3be966947bcc19d0b355ab7147edd37265b36d7
underengineering <san4a852b@gmail.com>
2024-06-28 18:18:18 +0300

    fix: Update for new nixpkgs

--- modules/system/wayland/default.nix
+++ modules/system/wayland/default.nix
@@ -38,10 +38,9 @@ in {
       glxinfo
     ];
 
-    hardware.opengl = {
+    hardware.graphics = {
       enable = true;
-      driSupport = true;
-      driSupport32Bit = true;
+      enable32Bit = true;
       extraPackages = with pkgs; [
         vaapiVdpau
         libvdpau-va-gl
