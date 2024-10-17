b951ca1486784d5690fda7f184f692eddbd2ef88
underengineering <san4a852b@gmail.com>
2024-10-18 02:13:10 +0300

    fix(nvim): Fix inverted statusline

--- config/nvim/lua/highlight.lua
+++ config/nvim/lua/highlight.lua
@@ -1,24 +1,6 @@
---[[ local get_color = require("utils").get_color
-local palette = require("gruvbox.palette").get_base_colors()
+local get_color = require("utils").get_color
 
--- Set floating window background color to the same color as buffer background
-vim.api.nvim_set_hl(0, "NormalFloat", {
-    fg = get_color("Normal", "fg#"),
-    bg = palette.bg1
+vim.api.nvim_set_hl(0, "StatusLine", {
+    fg = get_color("StatusLine", "fg#"),
+    bg = get_color("StatusLine", "bg#"),
 })
-
-vim.api.nvim_set_hl(0, "FloatBorder", {
-    fg = palette.bg2,
-    bg = get_color("FloatBorder", "bg#")
-})
-
-vim.api.nvim_set_hl(0, "LspInfoBorder", {
-    fg = palette.bg2,
-    bg = get_color("LspInfoBorder", "bg#")
-})
-
-vim.api.nvim_set_hl(0, "NoiceLspProgressTitle", {
-    fg = palette.fg0,
-    bg = get_color("NoiceLspProgressTitle", "bg#")
-})
-]]
