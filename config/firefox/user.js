1ea4e16215be9f63f4cdb28b3aaef51f99433cfc
underengineering <san4a852b@gmail.com>
2024-12-01 22:20:16 +0300

    feat: Add shyfox theme

--- config/firefox/user.js
+++ config/firefox/user.js
@@ -1,3 +1,27 @@
+/* * * * * SHYFOX * * * * */
+// userchrome.css usercontent.css activate
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+// disable new sidebar
+user_pref("sidebar.revamp", false);
+
+// Fill SVG Color
+user_pref("svg.context-properties.content.enabled", true);
+
+// Integrated calculator at urlbar
+user_pref("browser.urlbar.suggest.calculator", true);
+
+// Integrated unit convertor at urlbar
+user_pref("browser.urlbar.unitConversion.enabled", true);
+
+// GTK rounded corners
+user_pref("widget.gtk.rounded-bottom-corners.enabled", false);
+
+// Who is bogus? (fixes Sidebery tab dragging on Linux)
+user_pref("widget.gtk.ignore-bogus-leave-notify", 1);
+
+/* * * * * ARKENFOX * * * * */
+
 /* 1003: disable storing extra session data [SETUP-CHROME]
  * define on which sites to save extra session data such as form content, cookies and POST data
  * 0=everywhere, 1=unencrypted sites, 2=nowhere ***/
--- modules/users/wayland/applications/firefox/default.nix
+++ modules/users/wayland/applications/firefox/default.nix
@@ -24,20 +24,55 @@ in {
       type = types.str;
       default = "";
     };
-  };
-  config = mkIf (cfg.enable) {
-    programs.firefox = {
-      enable = true;
-      package = cfg.package;
-      profiles = {
-        main = {
-          extraConfig = (builtins.readFile "${inputs.arkenfox}/user.js") + cfg.extraConfig;
-        };
-        clean = {
-          id = 1;
-          extraConfig = cfg.extraConfig;
-        };
-      };
+    extraUserChrome = mkOption {
+      description = "CSS code that will be appended after shyfox";
+      type = types.str;
+      default = "";
     };
   };
+  config = let
+    firefoxConfigPath = ".mozilla/firefox";
+    mainProfileName = "main";
+  in
+    mkIf (cfg.enable) {
+      programs.firefox = {
+        enable = true;
+        package = cfg.package;
+        profiles = {
+          ${mainProfileName} = {
+            extraConfig = (builtins.readFile "${inputs.arkenfox}/user.js") + cfg.extraConfig;
+          };
+          clean = {
+            id = 1;
+            extraConfig = cfg.extraConfig;
+          };
+        };
+      };
+      # Link shyfox
+      home.file."${firefoxConfigPath}/${mainProfileName}/chrome/userChrome.css".text = ''
+        /* imports */
+        @import url("ShyFox/shy-variables.css");
+        @import url("ShyFox/shy-global.css");
+        @import url("ShyFox/shy-sidebar.css");
+        @import url("ShyFox/shy-toolbar.css");
+        @import url("ShyFox/shy-navbar.css");
+        @import url("ShyFox/shy-findbar.css");
+        @import url("ShyFox/shy-controls.css");
+        @import url("ShyFox/shy-compact.css");
+        @import url("ShyFox/shy-icons.css");
+        @import url("ShyFox/shy-floating-search.css");
+        ${cfg.extraUserChrome}
+      '';
+      home.file."${firefoxConfigPath}/${mainProfileName}/chrome/userContent.css".source = inputs.shyfox + "/chrome/userContent.css";
+      home.file."${firefoxConfigPath}/${mainProfileName}/chrome/ShyFox" = {
+        executable = false;
+        recursive = true;
+        source = "${inputs.shyfox}/chrome/ShyFox";
+      };
+      home.file."${firefoxConfigPath}/${mainProfileName}/chrome/icons" = {
+        executable = false;
+        recursive = true;
+        source = "${inputs.shyfox}/chrome/icons";
+      };
+    };
 }
